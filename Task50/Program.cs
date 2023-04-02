8649494dc91b49ca1b79b53e3056338c70d0c75d
NikolaiAbramov <abramovn85@mail.ru>
2023-04-02 18:05:19 +0700

    Task50 done

---
+++ Task50/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1 7 -> такого числа в массиве нет
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 10); // [1, 9]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.Write("Введите координаты a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < matrix.GetLength(0) && b < matrix.GetLength(1)) 
+  Console.WriteLine(matrix[a, b]);
+else 
+  Console.WriteLine($"{a}{b} -> такого числа в массиве нет");
